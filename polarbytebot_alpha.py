dc72352311ddbb092d622258920fdfbcdac1407a
Tinywave Netsphere <xyoc@hotmail.de>
2015-09-27 05:53:28 +0200

    refactored alpha version: distribute queues and processing missing

--- polarbytebot_alpha.py
+++ polarbytebot_alpha.py
@@ -57,7 +57,7 @@ class CommentQueue:
                         self.queue[cm.subreddit.display_name] = [cm]
                     else:
                         logging.info('commentQueue: add: {0} - {1}'\
-                                    .format(cm.name, int(cm.id,36))
+                                    .format(cm.name, int(cm.id,36)))
         
 class SubmissionQueue:
     """
@@ -82,13 +82,13 @@ class SubmissionQueue:
             logging.warning('SubmissionQueue: last_submission_id failed init: 0')
         while(True):
             subreddits = r.get_subreddit(self._enabled_subreddits)
-            submissions = subreddits.get_new(limit=None))
+            submissions = subreddits.get_new(limit=None)
             for subm in submissions:
                 if subm.author.name != self.botname:
                     if not self.first_submission:
                         if self.last_submission_id <= 0:
                             self.last_submission_id = int(cm.id,36)
-                            logging.warning('submissionQueue:'+\ 
+                            logging.warning('submissionQueue:'+\
                                             'last_submission_id repaired: {0}'\
                                             .format(int(cm.id,36)))
                         t_last_submission_id = int(cm.id,36)
@@ -102,7 +102,7 @@ class SubmissionQueue:
                         self.queue[subm.subreddit.display_name] = [subm]
                     else:
                         logging.info('submissionQueue: add: {0} - {1}'
-                                    .format(subm.name, int(subm.id,36))
+                                    .format(subm.name, int(subm.id,36)))
             
 class Polarbyte:
     def __init__(self, cfg_file):
@@ -111,7 +111,7 @@ class Polarbyte:
         self.signature = cfg_file['reddit']['signature']
         self.botname = cfg_file['oauth2']
         self.enabled_subreddits = [e.strip() for e in cfg_file['reddit']['enabled_subreddits'].split(',')]
-        authenticate()
+        self.authenticate()
 
     def authenticate(self):
         try:
@@ -127,18 +127,48 @@ class Polarbyte:
     def collect(self):
         cmQueue = CommentQueue(self.enabled_subreddits, self.botname)
         smQueue = SubmissionQueue(self.enabled_subreddits, self.botname)
-        
-        
-
-
-
+        #process objects
         updateLastestObjectIds(cmQueue.last_comment_id, smQueue.last_submission_id)
         session.commit()
+
     def submit(self):
+        submitComments()
+        submitSubmissions()
+
+    def submitComments(self):
+        to_be_commented = session.query(bot_comments).filter_by(submitted=False).all()
+        for tbcm in to_be_commented:
+            obj = r.get_info(thing_id=tbcm.thing_id)
+            if tbcm.thing_id[:2] == 't3':
+                reply_obj = obj.add_comment(tbcm.content)
+                updateSubmitted(bot_comments, tbcm.thing_id, reply_obj.name)
+                logging.info('submitComment: submit: {0}'.format(tbcm.id))
+            elif tbcm.thing_id[:2] == 't1':
+                reply_obj = obj.reply(tbcm.content)
+                updateSubmitted(bot_comments, tbcm.thing_id, reply_obj.name)
+                logging.info('submitComment: submit: {0}'.format(tbcm.id))
+            elif tbcm.thing_id[:1] == 'i':
+                new_id = searchSubmitted(bot_comments, tbcm.thing_id[1:])
+                if new_id != None:
+                    updateThingId(bot_comments, tbcm.id, new_id)
+            session.commit()
+
+    def submitSubmissions(self):
+        to_be_submitted = session.query(bot_submissions).filter_by(submitted=False).all()
+        for tbsm in to_be_submitted:
+            if tbsm.type == 'link':
+                sub_obj = r.submit(tbsm.subreddit, tbsm.title, url=tbsm.content)
+            elif tbsm.type == 'self':
+                sub_obj = r.submit(tbsm.subreddit, tbsm.title, text=tbsm.content)
+            session.query(bot_submissions).filter_by(id=tbsm.id).update({'submitted':True})
+            #updateSubmitted(bot_submissions, tbsm.thing_id, sub_obj.id)
+            logging.info('submitSubmission: submit: {0}'.format(tbsm.id))
+            session.commit()
+
     def addComment(self, _thing_id, _content, _submitted=False):
         last_id = _thing_id
         extra_len = len('\n\n--- continued below ---') + len(self.signature)
-        while(True)
+        while(True):
             if len(_content) <= 0:
                 return
             content_parts = _content.split('\n')
@@ -146,11 +176,20 @@ class Polarbyte:
             for part in content_parts:
                 if len(stiched_content) + len(part) + extra_len <= 10000:
                     stiched_content += part
-                
-                
-            current_content = _content[:10000 - extra_len]
-        row = bot_comments()
-        
+                else:
+                    break
+            row = bot_comments()
+            row.thing_id = last_id
+            row.submitted = _submitted
+            if _content[len(stiched_content):] == '':
+                row.content = stiched_content + self.signature
+            else:
+                row.content = stiched_content + '\n\n--- continued below ---' + self.signature
+            session.add(row)
+            session.commit()
+            last_id = 'i{}'.format(session.query(bot_comments).order_by(desc(bot_comments.id)).first().id)
+            _content = _content[len(stiched_content):]
+
     def addSubmission(self, _subreddit, _title, _author, _content, _type, _submitted=False):
         row = bot_submissions()
         if len(_title) + len(_author) + len(' []') > 300:
@@ -172,28 +211,18 @@ class Polarbyte:
             row.last_comment = _last_comment_id
             row.last_submission = _last_submission_id
             session.add(row)
-        else
+        else:
             session.query(subreddit).filter_by(website='reddit').first()\
                 .update({'last_submission':_last_submission_id,\
                          'last_comment':_last_comment_id})
+    def updateSubmitted(self, _table, _search_id, _submit_id):
+        session.query(_table).filter_by(id=_search_id)\
+            .update({'submitted':True,'submitted_id':_submit_id})
+    def searchSubmitted(self, _table, _search_id):
+        return session.query(_table).filter_by(id=_search_id).first().submitted_id
+    def updateThingId(self, _table, _search_id, _new_id):
+        session.query(_table).filter_by(id=_search_id).update({'thing_id':_new_id})
 
-def prepare_comment(_thing_id, _submitted, _content):
-    last_id = _thing_id
-    while(True):            
-        _current_content = _content[:10000 - len('\n\n--- continued below ---') - len(cfg_file['reddit']['signature'])]
-        if len(_content) == 0:
-            return
-        row = bot_comments()
-        row.thing_id = last_id
-        row.submitted = _submitted
-        if _content[len(_current_content):] == '':
-            row.content = _current_content + cfg_file['reddit']['signature']
-        else:
-            row.content = _current_content + '\n\n--- continued below ---' + cfg_file['reddit']['signature']
-        session.add(row)
-        session.commit()
-        last_id = 'i' + str(session.query(bot_comments).order_by(desc(bot_comments.id)).first().id)
-        _content = _content[len(_current_content):]
 def guildwars2_filter_cm(comments, array_anet_names):
     for cm in comments:
         logging.info("comment")
@@ -210,7 +239,7 @@ def guildwars2_filter_cm(comments, array_anet_names):
             row.submitted = False
             row.content = cm.permalink.replace('//www.reddit.com','//np.reddit.com') + '?context=1000'
             session.add(row)
-        continue # DISALLOWS COMMENTS TO BE PARSED FPR GW2 LINKS
+        continue # Dlogging.info('submitComment: submit: {0}'.format(tbcm.id))ISALLOWS COMMENTS TO BE PARSED FPR GW2 LINKS
         if re.search('http.*?:\/\/.*?guildwars2.com\/', cm.body) != None:
             logging.info("comment with gw2 link: " + cm.name)
             all_links = re.findall('http.*?:\/\/.*?guildwars2.com\/[^ \])\s]*', cm.body)
@@ -281,90 +310,10 @@ def distribute_queues(comment_queue, submission_queue):
     except Exception as e:
         logging.error(e)
 def main():
-    global r
-    global search_comment_id
-    global search_submission_id
-    
+    global r    
     logging.config.fileConfig(path_to_cfg)
-    
-    r = praw.Reddit(cfg_file['reddit']['user_agent'])	
-    enabled_subreddits = ['Guildwars2']
-    while (True):
-        try:
-            authenticate(cfg_file['oauth2'])
-        except Exception:
-            logging.error('OAuth2 could not authenticate')
-            continue
-        else:
-            logging.info('OAuth2 authenticated')
-            break;
 
-    while (True):
-        try:
-            subreddit_obj = session.query(subreddit).filter_by(website='reddit').first()
-            search_comment_id = subreddit_obj.last_comment
-            search_submission_id = subreddit_obj.last_submission
-        except:
-            logging.warning('search_comment_id and search_submission_id initialized with 0')
-            search_comment_id = 0
-            search_submission_id = 0
-
-        submission_queue = {}
-        comment_queue = {}
-        try:
-            comment_queue = load_recent_comments(enabled_subreddits)
-            submission_queue = load_recent_submissions(enabled_subreddits)
-            distribute_queues(comment_queue, submission_queue)
-            last_ids = session.query(subreddit).filter_by(website="reddit").first()
-            if last_ids == None:
-                row = subreddit()
-                row.website = 'reddit'
-                row.last_submission = search_submission_id
-                row.last_comment = search_comment_id
-                #logging.info('last submission updated: ' + str(search_submission_id) + ' - last comment updated: ' + str(search_comment_id))
-                session.add(row)
-            else:
-                session.query(subreddit).filter_by(website="reddit").update({'last_submission':search_submission_id,'last_comment':search_comment_id})
-                #logging.info('last submission updated: ' + str(search_submission_id) + ' - last comment updated: ' + str(search_comment_id))
-            session.commit()
-        except KeyboardInterrupt:
-            raise
-        except Exception as e:
-            logging.error(e)
-            session.rollback()
-        try:
-            to_be_commented = session.query(bot_comments).filter_by(submitted=False).all()
-            for tbcm in to_be_commented:
-                cm_obj = r.get_info(thing_id=tbcm.thing_id)
-                if tbcm.thing_id[:2] == 't3':
-                    reply_obj = cm_obj.add_comment(tbcm.content)
-                    session.query(bot_comments).filter_by(id=tbcm.id).update({'submitted':True,'submitted_id':reply_obj.name})
-                    logging.info(str(tbcm.id) + ' in bot_comments submitted')
-                elif tbcm.thing_id[:2] == 't1':
-                    reply_obj = cm_obj.reply(tbcm.content)
-                    session.query(bot_comments).filter_by(id=tbcm.id).update({'submitted':True,'submitted_id':reply_obj.name})
-                    logging.info(str(tbcm.id) + ' in bot_comments submitted')
-                elif tbcm.thing_id[:1] == 'i':
-                    new_id = session.query(bot_comments).filter_by(id=tbcm.thing_id[1:]).first().submitted_id
-                    if new_id != None:
-                        session.query(bot_comments).filter_by(id=tbcm.id).update({'thing_id':new_id})
-                session.commit()
-        except Exception as e:
-            logging.error(e)
-            session.rollback()
-        try:
-            to_be_submitted = session.query(bot_submissions).filter_by(submitted=False).all()
-            for tbsm in to_be_submitted:
-                if tbsm.type == 'link':
-                    r.submit(tbsm.subreddit,tbsm.title,url=tbsm.content)
-                elif tbsm.type == 'self':
-                    r.submit(tbsm.subreddit,tbsm.title,text=tbsm.content)
-                session.query(bot_submissions).filter_by(id=tbsm.id).update({'submitted':True})
-                logging.info(str(tbsm.id) + ' in bot_submissions submitted')
-                session.commit()
-        except Exception as e:
-            logging.error(e)
-            session.rollback()
+    bot = Polarbyte(cfg_file)
 
 if __name__ == '__main__':
     main()
